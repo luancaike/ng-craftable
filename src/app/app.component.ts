f7d96a84973361879fdfe2e831a6a0367617af27
luancaike <luan.caike@acad.ifma.edu.br>
2021-06-29 17:42:46 -0300

    feat: updates project of development

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
-import {Component, ViewChild} from '@angular/core';
+import {ChangeDetectionStrategy, Component, ViewChild} from '@angular/core';
 import {CraftableComponent} from 'ng-craftable';
 
 @Component({
     selector: 'app-root',
+    changeDetection: ChangeDetectionStrategy.OnPush,
     styleUrls: ['./app.component.scss'],
     template: `
         <div class="ddd-area">
             <div class="toolbar">
-                <div class="btn-group  mt-1 mx-1">
+                <div class="btn-group mt-2 mx-2">
                     <button type="button"
                             class="btn btn-sm btn-outline-primary"
                             [class.btn-primary]="craftable.enableDraw"
@@ -19,12 +20,12 @@ import {CraftableComponent} from 'ng-craftable';
                     <button type="button"
                             [class.btn-secondary]="visualizationMode"
                             [class.btn-outline-secondary]="!visualizationMode"
-                            class="btn" (click)="visualizationMode = !visualizationMode">
+                            class="btn btn-sm" (click)="visualizationMode = !visualizationMode">
                         <i class="bi bi-eye-fill"></i>
                         Visualization
                     </button>
                 </div>
-                <div class="btn-group  mt-1 mx-1">
+                <div class="btn-group  mt-2 mx-2">
                     <button type="button"
                             class="btn btn-sm btn-outline-secondary"
                             (click)="craftable.undo()">
@@ -34,6 +35,60 @@ import {CraftableComponent} from 'ng-craftable';
                             class="btn btn-sm btn-outline-secondary"
                             (click)="craftable.redo()">
                         <i class="bi bi-arrow-90deg-right"></i> Redo
+                    </button>
+                </div>
+                <div class="btn-group  mt-2 mx-2">
+                    <button type="button"
+                            class="btn btn-sm btn-outline-secondary"
+                            (click)="craftable.copy()">
+                        <i class="bi bi-files"></i>
+                        Copy
+                    </button>
+                    <button type="button"
+                            class="btn btn-sm btn-outline-secondary"
+                            (click)="craftable.cut()">
+                        <i class="bi bi-scissors"></i>
+                        Cut
+                    </button>
+                    <button type="button"
+                            class="btn btn-sm btn-outline-secondary"
+                            (click)="craftable.paste()">
+                        <i class="bi bi-clipboard"></i>
+                        Paste
+                    </button>
+                </div>
+                <div class="btn-group  mt-2 mx-2">
+                    <button type="button"
+                            class="btn btn-sm btn-outline-secondary"
+                            (click)="craftable.bringToFront()">
+                        <i class="bi bi-exclude"></i>
+                        Bring to Front
+                    </button>
+                    <button type="button"
+                            class="btn btn-sm btn-outline-secondary"
+                            (click)="craftable.bringToBack()">
+                        <i class="bi bi-intersect"></i>
+                        Bring to Back
+                    </button>
+                    <button type="button"
+                            class="btn btn-sm btn-outline-secondary"
+                            (click)="craftable.bringToForward()">
+                        <i class="bi bi-front"></i>
+                        Bring Forward
+                    </button>
+                    <button type="button"
+                            class="btn btn-sm btn-outline-secondary"
+                            (click)="craftable.bringToBackward()">
+                        <i class="bi bi-back"></i>
+                        Bring Backward
+                    </button>
+                </div>
+                <div class="btn-group  mt-2 mx-2">
+                    <button type="button"
+                            class="btn btn-sm btn-outline-danger"
+                            (click)="craftable.deleteSelection()">
+                        <i class="bi bi-trash-fill"></i>
+                        Delete
                     </button>
                 </div>
             </div>
