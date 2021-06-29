03d60695107c4521e8132d2d59cc680e0a2b452a
luancaike <luan.caike@acad.ifma.edu.br>
2021-06-29 16:23:50 -0300

    feat: updates project of development

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,71 @@
-import {Component} from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {CraftableComponent} from 'ng-craftable';
 
 @Component({
-  selector: 'app-root',
-  styleUrls: ['./app.component.scss'],
-  template: `
-    <div class="ddd-area">
-      <div class="toolbar">
-        <button type="button" (click)="enableDraw = !enableDraw">Enable Draw Mode</button>
-        <button type="button" (click)="visualizationMode = !visualizationMode">Enable Visualization Mode</button>
-      </div>
-      <div class="workspace">
-        <ng-craftable [enableDraw]="enableDraw" [drawItemData]="{teste: 'ABC'}" [visualizationMode]="visualizationMode">
-          <ng-template #template let-data>
-            {{data | json}}
-            <div *ngIf="data.teste === 'ABC'">
-              <button type="button">teste</button>
+    selector: 'app-root',
+    styleUrls: ['./app.component.scss'],
+    template: `
+        <div class="ddd-area">
+            <div class="toolbar">
+                <div class="btn-group  mt-1 mx-1">
+                    <button type="button"
+                            class="btn btn-sm btn-outline-primary"
+                            [class.btn-primary]="craftable.enableDraw"
+                            [class.btn-outline-primary]="!craftable.enableDraw"
+                            (click)="drawNewLego()">
+                        <i class="bi bi-pencil"></i>
+                        Draw
+                    </button>
+                    <button type="button"
+                            [class.btn-secondary]="visualizationMode"
+                            [class.btn-outline-secondary]="!visualizationMode"
+                            class="btn" (click)="visualizationMode = !visualizationMode">
+                        <i class="bi bi-eye-fill"></i>
+                        Visualization
+                    </button>
+                </div>
+                <div class="btn-group  mt-1 mx-1">
+                    <button type="button"
+                            class="btn btn-sm btn-outline-secondary"
+                            (click)="craftable.undo()">
+                        <i class="bi bi-arrow-90deg-left"></i> Undo
+                    </button>
+                    <button type="button"
+                            class="btn btn-sm btn-outline-secondary"
+                            (click)="craftable.redo()">
+                        <i class="bi bi-arrow-90deg-right"></i> Redo
+                    </button>
+                </div>
             </div>
-          </ng-template>
-        </ng-craftable>
-      </div>
-      <div class="sidebar"></div>
-    </div>
+            <div class="workspace">
+                <ng-craftable #craftable [visualizationMode]="visualizationMode">
+                    <ng-template #template let-data>
+                        <div class="card h-100">
+                            <div class="card-header">
+                                {{data.key}}
+                            </div>
+                            <div class="card-body">
+                                <p class="card-text">
+                                    {{data | json}}
+                                </p>
+                            </div>
+                            <div class="card-footer">
+                                <a href="#" *ngIf="data.teste === 'ABC'" class="btn btn-primary">Teste</a>
+                            </div>
+                        </div>
+                    </ng-template>
+                </ng-craftable>
+            </div>
+            <div class="sidebar"></div>
+        </div>
 
-  `
+    `
 })
 export class AppComponent {
-  enableDraw = false;
-  visualizationMode = false;
+    @ViewChild('craftable') craftable: CraftableComponent;
+    visualizationMode = false;
+
+    drawNewLego() {
+        this.craftable.drawNewLego({teste: 'ABC'});
+    }
 }
